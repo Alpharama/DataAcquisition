f877ec52f2182589071f00c6b4ec9259b3b1a5f1
Marama Simoneau <msimoneau@cf8e6a33-4240-4373-997a-adc33f62e4ed.fritz.box>
2025-07-29 10:00:26 -1000

    update script

--- script.py
+++ script.py
@@ -30,22 +30,18 @@ def run():
 
     results = []
     for token_data in tqdm(boosted_tokens):
-        token = {'chainId': token_data['chainId'], 'tokenAddress': token_data['tokenAddress']}
-        token_tuple = (token_data['chainId'], token_data['tokenAddress'])
-
-        if token_tuple in tokens_to_look:
-            if threshold_times[token_tuple] < int(time.time() * 1000) + 24 * 60 * 60 * 1000:
-                try:
+        try : 
+            token = {'chainId': token_data['chainId'], 'tokenAddress': token_data['tokenAddress']}
+            token_tuple = (token_data['chainId'], token_data['tokenAddress'])
+            if token_tuple in tokens_to_look:
+                if threshold_times[token_tuple] < int(time.time() * 1000) + 24 * 60 * 60 * 1000:
                     pair_data = get_pairs_by_token(token)
                     if pair_data:
                         results.append(pair_data)
-                except Exception as e:
-                    print(f"❌ Erreur lors de la récupération des paires : {e}")
+                else:
+                    tokens_to_look.remove(token_tuple)
             else:
-                tokens_to_look.remove(token_tuple)
-        else:
-            if token_tuple not in seen_tokens:
-                try:
+                if token_tuple not in seen_tokens:
                     pair_data = get_pairs_by_token(token)
                     if pair_data:
                         threshold_times[token_tuple] = pair_data['pairCreatedAt']
@@ -53,8 +49,8 @@ def run():
                             results.append(pair_data)
                             tokens_to_look.append(token_tuple)
                             seen_tokens.append(token_tuple)
-                except Exception as e:
-                    print(f"❌ Erreur lors de la récupération des paires : {e}")
+        except Exception as e:
+                print(f"❌ Erreur lors de la récupération des paires : {e}")
 
     if results:
         print(f"✅ Données sauvegardées ({len(results)} tokens)")
